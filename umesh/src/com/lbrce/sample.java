8c8fea31b1705df73c3f265ad5fa9b9c89f58e38
Admin <Admin@DESKTOP-UK9TSQ1>
2023-11-07 11:07:28 +0530

    pushed

---
+++ umesh/src/com/lbrce/sample.java
@@ -0,0 +1,10 @@
+package com.lbrce;
+
+public class sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is team leader");
+	}
+
+}
